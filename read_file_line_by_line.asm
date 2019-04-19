58c69ab8b8bfa3d0492ebc46af015a1c8755020c
Rafael G. de Paulo <rafael-g-depaulo@gmail.com>
2019-04-19 18:25:03 -0300

    commit inicial

---
+++ read_file_line_by_line.asm
@@ -0,0 +1,87 @@
+.data
+file_name: 		.asciiz "Code/test.txt"
+nada: 				.space 18
+file_buffer: 	.space 20
+file_buffer_length: .word 20
+line_buffer:	.space 81
+file_descriptor: .word 0
+newline:      .word '\n'
+
+.text
+# open file (do once)
+	la $a0, file_name
+
+	li   $v0, 13       						# system call for open file
+	la   $a0, file_name						# input file name
+	li   $a1, 0        						# flag for reading
+	li   $a2, 0        						# mode is ignored
+	syscall            						# open a file 
+	move $s0, $v0      						# save the file descriptor 
+	sw $s0, file_descriptor 			# save file descriptor on memory
+	la $s1, line_buffer 					# get start of line buffer ($s1 = line_buffer)
+
+# read 20 characters from file ####
+get_line_chars:
+	li   $v0, 14       						# system call for reading from file
+	move $a0, $s0      						# file descriptor 
+	la   $a1, file_buffer					# address of buffer from which to read
+	lw   $a2, file_buffer_length	# hardcoded buffer length
+	syscall            						# read from file
+
+# if read less than buffer_length chars
+	slt $t5, $v0, $a2 						# if read less than buffer_length chars, finished file
+	beq $t5, $zero, not_EOF				# set a flag to end procedure after processing line
+	
+	add $a1, $a1, $v0 						# end of characters read from file
+	li $t3, '\n'									# load '\n'
+	sb $t3, 0($a1)								# add a '\n' at the end of the buffer
+# end_if
+
+not_EOF:
+	# put file buffer content into line buffer (up to \n)
+	# if current byte not '\n', put into line buffer
+	la $t0, file_buffer 					# $t0 = inicio do file buffer
+	lw $t1, file_buffer_length  	# tamanho do buffer
+	add $t1, $t1, $t0							# $t1 = final do buffer
+	
+	li $t3, '\n'									# $t3 = '\n'
+
+# move string from buffer to line_buffer	
+move_char_buffer2line:
+	lbu $t2, 0($t0)								# get cuttent byte
+	beq $t2, $t3, line_done				# if found '\n', end line and proccess it
+	
+	# if not '\n', write to line buffer
+	sb $t2, 0($s1)									# write to current char in line
+	addi $s1, $s1, 1								# increase line buffer pointer ($s1)
+	addi $t0, $t0, 1								# increase file buffer pointer ($t0)
+	slt $t4, $t0, $t1								# if at the end of buffer
+	beq $t4, $zero, get_line_chars	# read next line
+	
+	j move_char_buffer2line					# read more chars into buffer
+	
+	# if '\n', process line
+line_done:
+
+	li $t3, '\0'									 # write '\0' to end line
+	sb $t3, 0($s1)								 # write '\0' to end line
+	
+	###### (TODO) process line
+	###### (TODO) process line
+
+	###### (TODO) ADD REST OF BUFFER TO NEXT LINE
+	###### (TODO) ADD REST OF BUFFER TO NEXT LINE do it thinking about below one
+	###### (TOODO) reset line pointer do it thinking about above one
+	###### (TOODO) reset line pointer
+	
+	# check $t5. if == 1, EOF reached, dont get another line
+	beq $t5, $zero, get_line_chars 	# get next line if not at EOF
+	
+	# closing file
+	lw $a0, file_descriptor  	# close file
+	li $v0, 16								# close file
+	syscall										# close file
+	
+	
+	
+# read file code from https://stackoverflow.com/questions/37469323/assembly-mips-read-text-from-file-and-buffer/37505359#37505359
