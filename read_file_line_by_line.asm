31bab2c341191a5c9439fd692739688a6aa89d7d
Rafael G. de Paulo <rafael-g-depaulo@gmail.com>
2019-04-20 21:07:51 -0300

    estruturei a leitura, agora é um procedimento de ordem maior

--- read_file_line_by_line.asm
+++ read_file_line_by_line.asm
@@ -9,7 +9,62 @@ newline:      			.word '\n'
 line:								.asciiz "linha: "
 
 .text
-# open file (do once)
+	
+	la $a0, file_name
+	la $a1, print_line
+	jal read_file_lines
+	
+	# end program
+	li $v0, 10
+	syscall
+
+# FUNCAO TESTE QUE PRINTA A STRING INSERIDA EM $a0
+print_line:
+	# push to stack
+	subi $sp, $sp, 12
+	sw $t0,  0($sp)
+	sw $a0,  8($sp)
+	sw $v0, 12($sp)
+	
+	move $t0, $a0
+	la $a0, line
+	li $v0, 4
+	syscall
+	move $a0, $t0
+	syscall
+	la $a0, newline
+	syscall
+	
+	# push to stack
+	lw $t0,  0($sp)
+	lw $a0,  8($sp)
+	lw $v0, 12($sp)
+	addi $sp, $sp, 12
+	
+	jr $ra # return
+		
+# FUNCAO QUE LE UM ARQUIVO LINHA A LINHA E EXECUTA A FUNCAO DADA EM CADA LINHA
+read_file_lines:
+# $a0: nome do arquivo
+# $a1: funcao a ser executada
+# returns: void
+
+	# push registers to the stack
+	subi $sp, $sp, 44
+	sw $a0, 0($sp)				# used for function calls
+	sw $a1, 4($sp)				# used for function calls
+	sw $v0, 8($sp)				# used for system calls
+	sw $t0, 12($sp)				# used for file buffer pointer
+	sw $t1, 16($sp)				# used for pointer to end of file buffer
+	sw $t2, 20($sp)				# used read/write bytes from file buffer to string buffer
+	sw $t3, 24($sp)				# used to store chars for comparison and insertion
+	sw $t4, 28($sp)				# used to store end of buffer flag
+	sw $t5, 32($sp)				# used to store EOF flag
+	sw $s0, 36($sp)				# used to store file descriptor
+	sw $s1, 40($sp)				# used for line buffer pointer
+	sw $ra, 44($sp)				# used for function calls
+
+	# open file (do once)
 	la $a0, file_name
 
 	li   $v0, 13       						# system call for open file
@@ -21,7 +76,7 @@ line:								.asciiz "linha: "
 	sw $s0, file_descriptor 			# save file descriptor on memory
 	la $s1, line_buffer 					# get start of line buffer ($s1 = line_buffer)
 
-# read 20 characters from file ####
+	# read 20 characters from file ####
 get_line_chars:
 	li   $v0, 14       						# system call for reading from file
 	move $a0, $s0      						# file descriptor 
@@ -29,14 +84,14 @@ get_line_chars:
 	lw   $a2, file_buffer_length	# hardcoded buffer length
 	syscall            						# read from file
 
-# if read less than buffer_length chars
+	# if read less than buffer_length chars
 	slt $t5, $v0, $a2 						# if read less than buffer_length chars, finished file
 	beq $t5, $zero, not_EOF				# set a flag to end procedure after processing line
 	
 	add $a1, $a1, $v0 						# end of characters read from file
 	li $t3, '\n'									# load '\n'
 	sb $t3, 0($a1)								# add a '\n' at the end of the buffer
-# end_if
+	# end_if
 
 not_EOF:
 	# put file buffer content into line buffer (up to \n)
@@ -67,17 +122,13 @@ line_done:
 	li $t3, '\0'									 # write '\0' to end line
 	sb $t3, 0($s1)								 # write '\0' to end line
 	
-	###### (TODO) process line
-	# printando pra tela como teste
-	la $a0, line
-	li $v0, 4
-	syscall
-	la $a0, line_buffer
-	syscall
-	la $a0, newline
-	syscall
-	###### (TODO) process line
-
+	###### CALL FUNCTION GIVEN, USING LINE BUFFER AS ARGUMENT
+	lw $t3, 4($sp)						# get function address
+	la $a0, line_buffer				# set up line buffer as argument
+	la $ra, return_from_call	# set up return address
+	jr $t3										# call function
+	return_from_call:					# return
+	
 	###### ADD REST OF BUFFER TO NEXT LINE & RESET LINE POINTER
 	# $t0: buffer pointer
 	# $s1: line buffer pointer
@@ -99,9 +150,24 @@ line_done:
 	lw $a0, file_descriptor  	# close file
 	li $v0, 16								# close file
 	syscall										# close file
+
+	# pop registers from the stack
+	lw $a0, 0($sp)				# used for function calls
+	lw $a1, 4($sp)				# used for function calls
+	lw $v0, 8($sp)				# used for system calls
+	lw $t0, 12($sp)				# used for file buffer pointer
+	lw $t1, 16($sp)				# used for pointer to end of file buffer
+	lw $t2, 20($sp)				# used read/write bytes from file buffer to string buffer
+	lw $t3, 24($sp)				# used to store chars for comparison and insertion
+	lw $t4, 28($sp)				# used to store end of buffer flag
+	lw $t5, 32($sp)				# used to store EOF flag
+	lw $s0, 36($sp)				# used to store file descriptor
+	lw $s1, 40($sp)				# used for line buffer pointer
+	lw $ra, 44($sp)				# used for function calls
+	addi $sp, $sp, 44
 	
-	# end program
-	li $v0, 10
-	syscall
-	
+	jr $ra
+	# return
+
+# END OF FUNCTION 
 # read file code from https://stackoverflow.com/questions/37469323/assembly-mips-read-text-from-file-and-buffer/37505359#37505359
